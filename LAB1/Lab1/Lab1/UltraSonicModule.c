0a425307e1f4136b5b7b746f0ed4c004202518a0
LuisHolchorV <luis.holchor@correounivalle.edu.co>
2025-04-01 23:34:07 -0500

    la

--- LAB1/Lab1/Lab1/UltraSonicModule.c
+++ LAB1/Lab1/Lab1/UltraSonicModule.c
@@ -16,7 +16,7 @@ void UltraSonicInit()
 
 float GetDistance()
 {
-	int counter = 0;
+	unsigned short int counter = 0;
 	
 	float distance = 0;
 	PORTC |= (1 << TRIGGER);
@@ -28,10 +28,10 @@ float GetDistance()
 	// Medir duraci?n del pulso (en us)
 	while (PINC & (1 << ECHO)) {//PIND & (1 << ECHO)
 		counter++;
-		_delay_us(12);  
+		_delay_us(10);  
 	}
 	// Calcular distancia (en cm)
-	distance = ((float)counter *12) / 58.0f;  // Formula: (us / 58) = cm
+	distance = ((float)counter *10) / 58.0f;  // Formula: (us / 58) = cm
 	
 	return distance;
 }
