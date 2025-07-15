87596cc210a375007fe3ee65ccdfd46f87ca01a4
LuisHolchorV <75144655+Rasorf3@users.noreply.github.com>
2025-07-15 12:14:50 -0500

    LAB4

---
+++ LAB4/LAB4/LAB4/main.c
@@ -0,0 +1,18 @@
+/*
+ * LAB4.c
+ *
+ * Created: 15/07/2025 12:14:19 p. m.
+ * Author : rasor
+ */ 
+
+
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
